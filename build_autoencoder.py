f6e35ccd4dec8af4554165f6ca90942f0848f8cf
hyve9 <afb8252@nyu.edu>
2024-04-28 17:17:31 -0400

    better preprocessing

--- build_autoencoder.py
+++ build_autoencoder.py
@@ -37,7 +37,7 @@ SHUFFLE_SIZE = 100
 # Dataset
 VAL_RATIO = 0.3
 
-def preprocess(audio, sample_rate, duration):
+def preprocess(audio, sequence_length, windows, freq_bins):
     logging.debug('Entering ' + sys._getframe().f_code.co_name)
     # For some reason Functional.call() is complaining about this
     audio = tf.reshape(audio, [-1, tf.shape(audio)[1]])
@@ -45,18 +45,20 @@ def preprocess(audio, sample_rate, duration):
     # Check for nans
     audio = tf.where(tf.math.is_nan(audio), tf.zeros_like(audio), audio)
 
-    # Check for short audio
-    target_length = duration * sample_rate
-    pad_length = target_length - tf.shape(audio)[1]
-    if pad_length > 0:
-        extra = pad_length + tf.shape(audio)[1] % WINDOW_LEN
-        full_pad_length = pad_length + extra
-        audio = tf.pad(audio, [[0, 0], [0, full_pad_length]], "CONSTANT")
+    # Check for incorrect audio length
+    offset = sequence_length - audio.shape[1]
+    if offset > 0:
+        logging.warning(f'Audio is too short, padding with {offset} zeros.')
+        audio = tf.pad(audio, [[0, 0], [0, offset]], "CONSTANT")
+    if offset < 0:
+        logging.warning(f'Audio is too long, truncating to {sequence_length} samples.')
+        audio = audio[:, :sequence_length]
 
     # Normalize audio between -1 and 1
     max_val = tf.reduce_max(tf.abs(audio), axis=1, keepdims=True)
-    max_val = tf.maximum(max_val, 1e-5)  # Prevent division by zero
+    max_val = tf.maximum(max_val, 1e-5)
     audio = audio / max_val
+
 
     # Convert to STFT
     S_audio = tf.signal.stft(audio, frame_length=WINDOW_LEN, frame_step=HOP_LEN, window_fn=WTYPE)
@@ -78,26 +80,23 @@ def preprocess(audio, sample_rate, duration):
     S_audio = tf.stack([reals, imags], axis=-1)
 
     # Make sure the shape matches windows, freq_bins, input_dim
-    windows = duration * sample_rate // HOP_LEN
-    freq_bins = WINDOW_LEN // 2 + 1 # should be constant, we recreate here just in case
     if S_audio.shape[1:] != tf.TensorShape([windows, freq_bins, 2]):
         logging.warning(f'Audio shape {S_audio.shape} does not match expected shape {[S_audio.shape[0], windows, freq_bins, 2]}')
         S_audio = tf.reshape(S_audio, [-1, windows, freq_bins, 2])
     return (S_audio, S_audio)
 
-def load_data(data_dir, sample_rate, duration, percentage=0.6):
+def load_data(data_dir, sequence_length, windows, freq_bins, percentage=0.6):
     logging.debug('Entering ' + sys._getframe().f_code.co_name)
     logging.warning('This function expects all audio to be preprocessed at 22 kHz.') 
     logging.warning('If this is not the case, you will likely get strange results.')
     # More efficient than using librosa and iterating over directories
-    ideal_sequence_length = sample_rate * duration - ((sample_rate * duration) % WINDOW_LEN)
     full_dataset = tf.keras.utils.audio_dataset_from_directory(
         directory=data_dir,
         batch_size=BATCH_SIZE,
         seed=0,
         labels=None,
         label_mode=None,
-        output_sequence_length=ideal_sequence_length,
+        output_sequence_length=sequence_length,
         subset=None)
 
     dataset_size = full_dataset.cardinality().numpy()
@@ -106,8 +105,12 @@ def load_data(data_dir, sample_rate, duration, percentage=0.6):
     # Use only a percentage of the dataset; full dataset keeps blowing up/exceeding memory
     full_dataset = full_dataset.take(use_size)
 
+    # Sometimes audio is empty; get rid of it
+    full_dataset = full_dataset.filter(lambda x: tf.size(x) > 0)
+
     # Preprocess data
-    full_dataset = full_dataset.map(lambda x: preprocess(x, sample_rate, duration), num_parallel_calls=tf.data.AUTOTUNE)
+    full_dataset = full_dataset.map(lambda x: preprocess(x, sequence_length, windows, freq_bins),
+                                    num_parallel_calls=tf.data.AUTOTUNE)
 
     # Split into training and validation
     val_size = int(VAL_RATIO * use_size)
@@ -130,59 +133,55 @@ def load_data(data_dir, sample_rate, duration, percentage=0.6):
 
 def build_model(windows, freq_bins, input_dim=2):
     logging.debug('Entering ' + sys._getframe().f_code.co_name)
-    model = Sequential()
+    model = Sequential([
+        # Encoder
+        Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(windows, freq_bins, input_dim)),
+        BatchNormalization(),
+        Conv2D(64, (3, 3), activation='relu', padding='same', strides=(2, 2)),
+        BatchNormalization(),
+        Conv2D(128, (3, 3), activation='relu', padding='same', strides=(2, 2)),
+        BatchNormalization(),
 
-    # Input layer
-    input_layer = Input(shape=(windows, freq_bins, input_dim))
-    model.add(input_layer)
-    
-    # Encoder
-    model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-    model.add(BatchNormalization())
-    model.add(Conv2D(64, (3, 3), activation='relu', padding='same', strides=(2, 2)))
-    model.add(BatchNormalization())
-    model.add(Conv2D(128, (3, 3), activation='relu', padding='same', strides=(2, 2)))
+        # Bottleneck
+        Conv2D(256, (3, 3), activation='relu', padding='same'),
 
-    # Bottleneck
-    model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+        # Decoder
+        Conv2DTranspose(128, (3, 3), activation='relu', padding='same', strides=(2, 2)),
+        BatchNormalization(),
+        Conv2DTranspose(64, (3, 3), activation='relu', padding='same', strides=(2, 2)),
+        BatchNormalization(),
+        Conv2DTranspose(32, (3, 3), activation='relu', padding='same'),
+        BatchNormalization(),
 
-    # Decoder
-    model.add(Conv2DTranspose(128, (3, 3), strides=(2, 2), activation='relu', padding='same'))
-    model.add(BatchNormalization())
-    model.add(Conv2DTranspose(64, (3, 3), strides=(2, 2), activation='relu', padding='same'))
-    model.add(BatchNormalization())
-    model.add(Conv2DTranspose(32, (3, 3), activation='relu', padding='same'))
+        # Output layer
+        Conv2D(input_dim, (3, 3), activation='linear', padding='same'),
 
-    # Output layer
-    model.add(Conv2D(input_dim, (3, 3), activation='linear', padding='same'))
-
-    # Output layer does not match input shape for some reason
-    model.add(Cropping2D(cropping=((0, 1), (0, 3)))) 
+        # Output layer does not match input shape for some reason
+        Cropping2D(cropping=((0, 3), (0, 3)))
+    ])
 
     model.compile(optimizer=Adam(), loss='mse')
     return model
 
 # Below debugging functions courtesy of Liqian :)
-def test_preprocess_function(sample_rate, duration):
+def test_preprocess_function(sequence_length, windows, freq_bins):
     logging.debug('Entering ' + sys._getframe().f_code.co_name)
-    test_input, _ = librosa.load('data/fma_small/006/006329.wav.wav', sr=sample_rate)
+    test_input, _ = librosa.load('data/fma_small/006/006329.wav.wav', sr=SAMPLE_RATE)
     # make sure duration is accurate
-    if len(test_input) < duration * sample_rate:
+    if len(test_input) < sequence_length:
         # pad
-        test_input = np.pad(test_input, (0, duration * sample_rate - len(test_input)))
-    elif len(test_input) > duration * sample_rate:
-        test_input = test_input[:duration * sample_rate]
-    if len(test_input) % WINDOW_LEN != 0:
-        test_input = test_input[:-(len(test_input) % WINDOW_LEN)]
+        test_input = np.pad(test_input, (0, sequence_length - len(test_input)))
+    elif len(test_input) > sequence_length:
+        test_input = test_input[:sequence_length]
     test_input[len(test_input)//2] = float('nan')
     test_input = tf.convert_to_tensor(test_input)
     # Add dims to match dataset
     test_input = tf.reshape(test_input, [1, -1, 1])
     # preprocess
-    processed_input = preprocess(test_input, sample_rate, duration)
+    processed_input = preprocess(test_input, sequence_length, windows, freq_bins)
     # Check if any NaNs remain
     contains_nan = tf.reduce_any(tf.math.is_nan(processed_input[0]))
-    logging.debug(f'Processed input: {processed_input[0].numpy()}')
+    # logging.debug(f'Processed input: {processed_input[0].numpy()}')
     logging.debug(f'Contains NaN: {contains_nan.numpy()}')
 
 class CheckNan(Layer):
@@ -220,23 +219,28 @@ if __name__ == '__main__':
     duration = args.duration
     percentage = args.percentage if args.percentage else 0.6
     logging.debug(f'Duration set to {duration}')
+    if duration < 1:
+        logging.error('Duration must be at least 1 second.')
+        sys.exit(1)
+
+    # Model params
+    sequence_length = duration * SAMPLE_RATE - ((duration * SAMPLE_RATE) % WINDOW_LEN)
+    windows = ((sequence_length - WINDOW_LEN) // HOP_LEN) + 1
+    freq_bins = WINDOW_LEN // 2 + 1
 
     # Test the preprocess function
     if loglevel == 'debug':
-        test_preprocess_function(SAMPLE_RATE, duration)
+        test_preprocess_function(sequence_length, windows, freq_bins)
     
     # Load audio and split into datasets
     # Not using test atm
-    train, val, _ = load_data(data_dir, SAMPLE_RATE, duration, percentage)
+    train, val, _ = load_data(data_dir, sequence_length, windows, freq_bins, percentage)
     
     # look at data to make sure we aren't crazy
     if loglevel == 'debug':
         pass
 
     # Build the autoencoder
-    # Model params
-    windows = duration * SAMPLE_RATE // HOP_LEN
-    freq_bins = WINDOW_LEN // 2 + 1
     autoencoder = build_model(windows, freq_bins)
 
     # look at the model
