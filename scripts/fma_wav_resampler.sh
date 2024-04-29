379c157cbfeb23af4aa648d26c09570e0e94a49b
hyve9 <afb8252@nyu.edu>
2024-04-29 13:52:57 -0400

    update scripts again?

--- scripts/fma_wav_resampler.sh
+++ scripts/fma_wav_resampler.sh
@@ -11,6 +11,7 @@ data_dir=$1
 
 for dir in $(ls -d $data_dir*); do
        for file in $(ls $dir); do
+                # Should convert to 22 kHz and mono
                 ffmpeg -i "$dir/$file" -ac 1 -ar 22050 "$dir/temp_$file" || true;
                 mv "$dir/temp_$file" "$dir/$file";
         done
